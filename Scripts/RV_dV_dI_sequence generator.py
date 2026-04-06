2ee37983b8230a511e606ec8f439689c86ac050b
yoavsharaby <yoavsharaby@mail.tau.ac.il>
2026-04-06 11:15:15 +0300

    Orginiazed files to Scripts folder

--- Scripts/RV_dV_dI_sequence generator.py
+++ Scripts/RV_dV_dI_sequence generator.py
@@ -1,5 +1,6 @@
+import numpy as np
+
 def generate_measurement_sequence(filename, target_voltages,voltage_step,sweep_mode, dc_bias_targets, aux_step_mv):
-    sequence = []
     with open(filename, 'w') as f:
         for v_target in target_voltages:
             # RV Section
@@ -21,7 +22,7 @@ def generate_measurement_sequence(filename, target_voltages,voltage_step,sweep_m
 
 
 # --- USER INPUTS ---
-file_name = "sequence.txt"
+file_name = "RV dV_dI sequence.txt"
 voltages = [5,4,3,2,1.5,1,0.75,0.5,0.25,0,-0.25,-0.5,-0.75,-1,-1.5,-2,-3,-4,-5]     # Define your order and numbers here
 #voltages = voltages[::-1]                                                            # in case the array needs reversal
 voltage_step = 5                                                                    # voltage sweep step size mV
