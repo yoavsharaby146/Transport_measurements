4af6e87423e9286b954ba46a1ab1620c840caaa0
yoavsharaby <yoavsharaby@mail.tau.ac.il>
2026-05-09 23:11:02 +0300

    fixed ramping time for two gate map

--- ICE version/procedures/resistance_two_gate_map.py
+++ ICE version/procedures/resistance_two_gate_map.py
@@ -161,13 +161,13 @@ class Resistance_two_gate_mapping_measurement(Procedure):
         point_counter = 0
 
         log.info(f"Moving to start: Slow={self.slow_start}V, Fast={self.fast_start}V")
-        slow_gate.voltage_ramping(self.slow_start, 2, 0.1)
+        slow_gate.voltage_ramping(self.slow_start, 2, 0.001)
         if self.should_stop():
             log.warning("User stopped measurement during initial slow SMU ramp")
             return
         time.sleep(self.long_delay)
 
-        fast_gate.voltage_ramping(self.fast_start, 2, 0.1)
+        fast_gate.voltage_ramping(self.fast_start, 2, 0.001)
         if self.should_stop():
             log.warning("User stopped measurement during initial fast SMU ramp")
             return
@@ -175,13 +175,13 @@ class Resistance_two_gate_mapping_measurement(Procedure):
         time.sleep(self.long_delay)
 
         for i, slow_v in enumerate(slow_range):
-            slow_gate.ramp_voltage(slow_v, 5, 0.01)
+            slow_gate.ramp_voltage(slow_v, 5, 0.001)
             time.sleep(self.short_delay)
 
             if self.scan_mode == 'Snake':
                 current_range = fast_range_forward if i % 2 == 0 else fast_range_backward
                 for fast_v in current_range:
-                    fast_gate.ramp_voltage(fast_v, 5, 0.05)
+                    fast_gate.ramp_voltage(fast_v, 5, 0.001)
                     time.sleep(self.short_delay)
                     data = self.getmeas(time_0)
                     self.emit('results', dict(zip(self.DATA_COLUMNS, data)))
@@ -193,7 +193,7 @@ class Resistance_two_gate_mapping_measurement(Procedure):
 
             elif self.scan_mode == 'Forward/Backward':
                 for fast_v in fast_range_forward:
-                    fast_gate.ramp_voltage(fast_v, 5, 0.05)
+                    fast_gate.ramp_voltage(fast_v, 5, 0.001)
                     time.sleep(self.short_delay)
                     data = self.getmeas(time_0)
                     self.emit('results', dict(zip(self.DATA_COLUMNS, data)))
@@ -204,7 +204,7 @@ class Resistance_two_gate_mapping_measurement(Procedure):
                         return
 
                 for fast_v in fast_range_backward:
-                    fast_gate.ramp_voltage(fast_v, 2, 0.005)
+                    fast_gate.ramp_voltage(fast_v, 2, 0.001)
                     time.sleep(self.short_delay)
                     data = self.getmeas(time_0)
                     self.emit('results', dict(zip(self.DATA_COLUMNS, data)))
